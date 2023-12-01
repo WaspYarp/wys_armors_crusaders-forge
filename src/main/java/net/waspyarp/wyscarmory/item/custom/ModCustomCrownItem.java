255867c73f9b3f4598373c435766ce0ad94f2e09
Wasp Yarp <w.waspyarp@gmail.com>
2023-12-01 20:07:08 -0300

    finaly ended the diamond and netherite tier texture, and modified his model

--- src/main/java/net/waspyarp/wyscarmory/item/custom/ModCustomCrownItem.java
+++ src/main/java/net/waspyarp/wyscarmory/item/custom/ModCustomCrownItem.java
@@ -1,14 +1,18 @@
 package net.waspyarp.wyscarmory.item.custom;
 
 import net.minecraft.world.item.Item;
-
+import net.minecraft.world.item.ItemStack;
 
 public class ModCustomCrownItem extends Item {
     public ModCustomCrownItem(Properties pProperties) {
         super(pProperties);
     }
 
+    @Override
+    public boolean hasCraftingRemainingItem(ItemStack stack) {
 
 
+
+        return super.hasCraftingRemainingItem(stack);
+    }
 }
-
