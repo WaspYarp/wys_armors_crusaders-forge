3221c31d5d62468b0c2bcc00bf0c4b10c2d151fa
Wasp Yarp <w.waspyarp@gmail.com>
2023-12-02 18:40:03 -0300

    minimal changes with to correct visual bugs

--- src/main/java/net/waspyarp/wyscarmory/item/custom/ModCustomTemplateItem.java
+++ src/main/java/net/waspyarp/wyscarmory/item/custom/ModCustomTemplateItem.java
@@ -1,8 +1,13 @@
 package net.waspyarp.wyscarmory.item.custom;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 
 
 public class ModCustomTemplateItem extends Item {
@@ -11,6 +16,12 @@ public class ModCustomTemplateItem extends Item {
     }
 
 
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.wyscarmory.templates_desc_1"));
+        pTooltipComponents.add(Component.translatable("tooltip.wyscarmory.templates_desc_2"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
 }
 
 
