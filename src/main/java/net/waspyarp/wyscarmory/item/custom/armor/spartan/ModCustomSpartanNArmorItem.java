2fb6a4a6c1f23c1b2116a47adcfa32b8cdf5fbb3
Wasp Yarp <w.waspyarp@gmail.com>
2023-12-02 18:41:14 -0300

    for this all of you gonna wait till the 2.0

---
+++ src/main/java/net/waspyarp/wyscarmory/item/custom/armor/spartan/ModCustomSpartanNArmorItem.java
@@ -0,0 +1,49 @@
+package net.waspyarp.wyscarmory.item.custom.armor.spartan;
+
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import net.waspyarp.wyscarmory.client.armor.crusader.CrusaderNArmorRender;
+import net.waspyarp.wyscarmory.item.custom.armor.ModCustomArmorItem;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ModCustomSpartanNArmorItem extends ModCustomArmorItem {
+
+
+    public ModCustomSpartanNArmorItem(ArmorMaterial material, Type type, Properties properties) {
+        super(material, type, properties);
+    }
+
+    @Override
+    public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+        consumer.accept(new IClientItemExtensions() {
+            private CrusaderNArmorRender renderer;
+
+            @Override
+            public @NotNull HumanoidModel<?> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack,
+                                                                   EquipmentSlot equipmentSlot, HumanoidModel<?> original) {
+                if (this.renderer == null)
+                    this.renderer = new CrusaderNArmorRender();
+
+                this.renderer.prepForRender(livingEntity, itemStack, equipmentSlot, original);
+                return this.renderer;
+            }
+        });
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.wyscarmory.netherite_tier_custom_armor"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+}
